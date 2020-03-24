f1600ec599d0e7c9ddde199a9a78464629d8b3f1
nandrease <neeme.andreas.eller@gmail.com>
2020-03-24 22:57:14 +0200

    PasswordMatch validator

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { PasswordMatch } from "../validators/password-match";
 
 @Component({
   selector: "app-signup",
@@ -7,22 +8,27 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
   styleUrls: [ "./signup.component.css" ]
 })
 export class SignupComponent implements OnInit {
-  authForm = new FormGroup({
-    username: new FormControl("", [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(20),
-      Validators.pattern(/^[a-z0-9]+$/)
-    ]),
-    password: new FormControl("", [ Validators.required, Validators.minLength(4), Validators.maxLength(20) ]),
-    passwordConfirmation: new FormControl("", [
-      Validators.required,
-      Validators.minLength(4),
-      Validators.maxLength(20)
-    ])
-  });
+  authForm = new FormGroup(
+    {
+      username: new FormControl("", [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20),
+        Validators.pattern(/^[a-z0-9]+$/)
+      ]),
+      password: new FormControl("", [ Validators.required, Validators.minLength(4), Validators.maxLength(20) ]),
+      passwordConfirmation: new FormControl("", [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(20)
+      ])
+    },
+    {
+      validators: [ this.passowrdMatch.validate ]
+    }
+  );
 
-  constructor() {}
+  constructor(private passowrdMatch: PasswordMatch) {}
 
   ngOnInit(): void {}
 }
