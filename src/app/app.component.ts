991a4c06013d190db028d12c1b1c9287f2aac478
nandrease <neeme.andreas.eller@gmail.com>
2020-03-25 10:17:28 +0200

    Signed in with BehaviorSubject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { AuthService } from "./auth/auth.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: [ "./app.component.css" ]
 })
 export class AppComponent {
-  title = 'emails';
+  signedIn = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.signedIn$.subscribe(signedIn => {
+      this.signedIn = signedIn;
+    });
+  }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject } from "rxjs";
+import { tap } from "rxjs/operators";
 
 interface UsernameAvailableResponse {
   available: boolean;
@@ -18,6 +20,8 @@ interface SignupResponse {
 })
 export class AuthService {
   rootUrl = "https://api.angular-email.com";
+  signedIn$ = new BehaviorSubject(false);
+
   constructor(private http: HttpClient) {}
 
   usernameAvailable(username: string) {
@@ -27,6 +31,10 @@ export class AuthService {
   }
 
   signup(credentials: SignupCedentials) {
-    return this.http.post<SignupResponse>(this.rootUrl + "/auth/signup", credentials);
+    return this.http.post<SignupResponse>(this.rootUrl + "/auth/signup", credentials).pipe(
+      tap(() => {
+        this.signedIn$.next(true);
+      })
+    );
   }
 }
