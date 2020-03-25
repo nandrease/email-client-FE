bea53277a6a632816bb9999231399292aca7420d
nandrease <neeme.andreas.eller@gmail.com>
2020-03-25 09:52:26 +0200

    handle signup in auth.service

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { PasswordMatch } from "../validators/password-match";
 import { UniqueUser } from "../validators/unique-user";
+import { AuthService } from "../auth.service";
 
 @Component({
   selector: "app-signup",
@@ -28,7 +29,28 @@ export class SignupComponent implements OnInit {
     }
   );
 
-  constructor(private passowrdMatch: PasswordMatch, private uniqueUser: UniqueUser) {}
+  constructor(private passowrdMatch: PasswordMatch, private uniqueUser: UniqueUser, private authService: AuthService) {}
 
   ngOnInit(): void {}
+
+  onSubmit() {
+    if (this.authForm.invalid) {
+      return;
+    }
+
+    this.authService.signup(this.authForm.value).subscribe({
+      next: response => {
+        // Navigate to some other route
+      },
+      error: err => {
+        // Internet problems
+        if (err.status === 0) {
+          this.authForm.setErrors({ noConnection: true });
+          return;
+        }
+        // Generic errors
+        this.authForm.setErrors({ unknownError: true });
+      }
+    });
+  }
 }
