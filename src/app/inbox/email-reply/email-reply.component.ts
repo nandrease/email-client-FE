65a1aff80fa7f706271344bece013a6ca8dbc8dc
nandrease <neeme.andreas.eller@gmail.com>
2020-03-26 21:24:05 +0200

    Send reply email

--- src/app/inbox/email-reply/email-reply.component.ts
+++ src/app/inbox/email-reply/email-reply.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, Input } from "@angular/core";
+import { EmailService } from "../email.service";
+import { Email } from "../email";
 
 @Component({
-  selector: 'app-email-reply',
-  templateUrl: './email-reply.component.html',
-  styleUrls: ['./email-reply.component.css']
+  selector: "app-email-reply",
+  templateUrl: "./email-reply.component.html",
+  styleUrls: [ "./email-reply.component.css" ]
 })
-export class EmailReplyComponent implements OnInit {
+export class EmailReplyComponent implements OnChanges {
+  @Input() email: Email;
+  showModal = false;
 
-  constructor() { }
+  constructor(private emailService: EmailService) {}
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
+    this.email = {
+      ...this.email,
+      from: this.email.to,
+      to: this.email.from,
+      subject: "RE: " + this.email.subject,
+      text: `\n\n-------- from ${this.email.from}\n${this.email.text}`
+    };
   }
 
+  onSubmit(email: Email) {
+    this.emailService.sendEmail(email).subscribe(() => {
+      this.showModal = false;
+    });
+  }
 }
